544387c56ec297b7d51b9124850a88dcf72cfc6b
monacorona <monacoronababy@gmail.com>
2017-03-03 20:32:49 -0800

    Ran schema.sql with some bugs to iron out.

--- server/schema.sql
+++ server/schema.sql
@@ -4,12 +4,29 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
-);
+  messageId varchar(10),
+  message varchar(10),
+  userId varchar(10),
+  roomId varchar(10),
+  primary key (messageId),
+  foreign key(userId) references users(userId),
+  foreign key(roomId) references room(roomId)
+)
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  /* Describe your table here.*/
+  userId varchar(10),
+  roomName varchar(10),
+  primary key (userId), 
+)
 
-
-
+CREATE TABLE room (
+  /* Describe your table here.*/
+  roomId varchar(10),
+  roomName varchar(10),
+  primary key (roomId),
+)
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
